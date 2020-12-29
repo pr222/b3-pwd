c1c1973b781388dcae16851622597eed9b713086
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-12-29 13:22:11 +0100

    Add submittion for username and message text

--- src/components/a-chatty-app/a-chatty-app.js
+++ src/components/a-chatty-app/a-chatty-app.js
@@ -13,9 +13,32 @@ const SOCKET = new WebSocket('wss://cscloud6-127.lnu.se/socket/')
 const template = document.createElement('template')
 template.innerHTML = `
 <style>
+:host {
+  display: block;
+}
+
+.hidden {
+  display: none;
+}
 </style>
 
 <p>A chatty app here!</p>
+<form id="nameForm" class="hidden">
+    <h1>Choose a username:</h1>
+    <input type="text" id="name" autofocus autocomplete="off">
+    <input type="submit" value="Submit">
+</form>
+
+<div id="chat">
+  <div id="sentMessages"></div>
+  <form id="messageForm">
+    <label for="writtenMessage">Write something in the chat:</label>
+
+    <textarea id="writtenMessage" name="writtenMessage" rows="3" cols="30"></textarea>
+
+    <input type="submit" value="Send">
+  </form>
+</div>
 `
 /**
  * Define the custom element.
@@ -35,23 +58,41 @@ customElements.define('a-chatty-app',
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
 
-      // ADD STUFF HERE
+      // Username
+      this._user = ''
+
+      // Select elements from shadow.
+      this._nameForm = this.shadowRoot.querySelector('#nameForm')
+      this._messageForm = this.shadowRoot.querySelector('#messageForm')
+
+      // Bindings for reaching this shadow.
     }
 
     /**
      * Called when the element has been insterted into the DOM.
      */
     connectedCallback () {
+      // Check wether there are already a registered user.
+      // If not, start application with a name submit form.
+      this._checkForUser()
+      // Adding event listeners
       SOCKET.addEventListener('open', this._opendedSocket)
       SOCKET.addEventListener('message', this._message)
+      SOCKET.addEventListener('error', this._error)
+      this._nameForm.addEventListener('submit', this._submitName)
+      this._messageForm.addEventListener('submit', this._sendMessage)
     }
 
     /**
      * Called when the element has been removed from the DOM.
      */
     disconnectedCallback () {
+      // Removing event listeners.
       SOCKET.removeEventListener('open', this._opendedSocket)
       SOCKET.removeEventListener('message', this._message)
+      SOCKET.removeEventListener('error', this._error)
+      this._nameForm.removeEventListener('submit', this._submitName)
+      this._messageForm.addEventListener('submit', this._sendMessage)
     }
 
     /**
@@ -68,10 +109,68 @@ customElements.define('a-chatty-app',
      *
      * @param {Event} event - a message sent.
      */
-    _message (event) {
+    async _message (event) {
+      const messageObject = await JSON.parse(event.data)
+
+      console.log(messageObject)
+
+    //   console.log(`User: ${messageObject.username}`)
+    //   console.log(`Type: ${messageObject.type}`)
+    //   console.log(`Message: ${messageObject.data}`)
+    }
+
+    /**
+     * Handling error event.
+     *
+     * @param {Event} event - an error event.
+     */
+    _error (event) {
+      console.log('An error occured...')
       console.log(event)
-      console.log(event.data)
-      console.log('A message was sent...')
+    }
+
+    /**
+     * Handling submitting of username.
+     *
+     * @param {Event} event - submit event.
+     */
+    _submitName (event) {
+      // Prevent submitting the form.
+      event.preventDefault()
+
+      console.log(this.name.value)
+      this._user = this.name.value
+
+      document.cookie = `username=${this._user}`
+    }
+
+    /**
+     * Handle new submitted message.
+     *
+     * @param {Event} event - submit event.
+     */
+    _sendMessage (event) {
+      // Prevent submitting the form.
+      event.preventDefault()
+      console.log('message sent')
+      console.log(this.writtenMessage.value)
+
+      // Empty the textfield for next message.
+      this.writtenMessage.value = ''
+      this.writtenMessage.focus()
+    }
+
+    /**
+     * Check for existing user.
+     */
+    _checkForUser () {
+      if (document.cookie) {
+        console.log(document.cookie)
+      } else {
+        console.log('No cookie...')
+        // Display name submission form.
+        this._nameForm.classList.remove('hidden')
+      }
     }
   }
 )
