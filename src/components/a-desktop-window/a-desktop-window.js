5a91f139a2f21079e9761ae941d38f8d7544d3c9
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-01-10 14:42:47 +0100

    Add images

--- src/components/a-desktop-window/a-desktop-window.js
+++ src/components/a-desktop-window/a-desktop-window.js
@@ -5,11 +5,51 @@
  * @version 1.0.0
  */
 
+const resize = (new URL('img/resize-button.png', import.meta.url)).href
+
 /**
  * Define the HTML template
  */
 const template = document.createElement('template')
 template.innerHTML = `
 <style>
+
+#resizeWrapper {
+    max-width: 30px;
+}
+
+img {
+    width: 100%;
+}
 </style>
+
+<div id="resizeWrapper">
+<img src="${resize}" alt="A resize app window button">
+</div>
 `
+/**
+ * Define the custom element.
+ */
+customElements.define('a-desktop-window',
+  /**
+   * Anonymous class of this element.
+   */
+  class extends HTMLElement {
+    /**
+     * Makes an instance of this type.
+     */
+    constructor () {
+      super()
+
+      // Attach shadow and append template.
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+
+      // Select elements from shadow.
+
+      // Bindings for reaching this shadow.
+    }
+
+    // TODO: Methods
+  }
+)
